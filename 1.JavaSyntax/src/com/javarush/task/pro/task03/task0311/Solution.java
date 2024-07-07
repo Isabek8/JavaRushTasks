5538df620948cd41c57d0d517a0683e484db9999
isabek8 <isabekozuev@gmail.com>
2024-07-07 21:38:48 +0300

    Added solution for javaSyntax task0311

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -9,8 +9,13 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner s = new Scanner(System.in);
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+        if (Math.abs(a - b) < 0.000001)
         System.out.println("числа равны");
         //напишите тут ваш код
+        else
         System.out.println("числа не равны");
     }
 }
