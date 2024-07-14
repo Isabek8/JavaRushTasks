d1c1d4c82d376883b9972c06c11d165448d8bf1e
isabek8 <isabekozuev@gmail.com>
2024-07-14 23:17:07 +0300

    Added solution for javaSyntax task0411 используя цикл for вывел четные числа на экран

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0411;
+
+/* 
+Четные числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a  = 0;
+        for (int i = 0; i < 15; i ++){
+            if ( i%2 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
