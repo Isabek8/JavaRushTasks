e8315d09f2f585878718de6e48ebc7982313f2e2
isabek8 <isabekozuev@gmail.com>
2024-07-16 23:57:23 +0300

    Added solution for javaSyntax task0405

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -7,11 +7,23 @@ package com.javarush.task.pro.task04.task0405;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        for (int i = 0; i < 10 ; i++){
-            for (int j = 0; j < 20; j++ ){
-                System.out.print("B");
+        int height = 0;
+
+        while (height < 10) {
+            int width = 0;
+            while (width < 20) {
+                if (height == 0 || height == 9) {
+                    System.out.print("Б");
+                } else if (width == 0 || width == 19) {
+                    System.out.print("Б");
+                } else {
+                    System.out.print(" ");
+                }
+                width++;
             }
             System.out.println();
+            height++;
+
         }
 
     }
