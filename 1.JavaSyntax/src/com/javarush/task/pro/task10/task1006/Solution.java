75210f96c7c24e9f809d1beb8427a7532ee597e1
youngSorcerer <isabekozuev@gmail.com>
2025-03-12 00:44:35 +0300

    Added solution for javaSyntax task 1006

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+       Car hybridCar = new HybridCar();
+       Car gasCar = new GasCar();
+       Car electricCar = new ElectricCar();
+    }
+}
