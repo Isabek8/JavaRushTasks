69101ca4a803dc7c2fcd45aa3429d36ee0496cf0
youngSorcerer <isabekozuev@gmail.com>
2025-03-11 23:10:54 +0300

    Added solution for javaSyntax task 1001

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task10.task1001;
+
+/* 
+Создадим небоскреб
+*/
+
+public class Skyscraper {
+    String sky;
+
+    //напишите тут ваш код
+    public Skyscraper(){
+        sky = "Небоскреб построен.";
+        System.out.println(sky);
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Skyscraper skyscraper = new Skyscraper();
+
+    }
+}
